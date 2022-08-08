13630877ab56223c9f111f195c001d490978da87
Oluwaseyi0910 <110403016+Oluwaseyi0910@users.noreply.github.com>
2022-08-09 00:39:37 +0100

    Add project files.

---
+++ ArrayandMatrixAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ArrayandMatrixAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] myArray = new int[3,3,3]
+            {
+                { {0,0,1}, {0,1,0}, {1,0,0} },
+                { {0,0,1}, {0,1,0}, {1,0,0} },
+                { {0,0,1}, {0,1,0}, {1,0,0} }
+            };
+
+            int[,,] arrayTwo = new int[3, 3, 3]
+            {
+                { {0,0,1}, {0,1,0}, {1,0,0} },
+                { {0,0,1}, {0,1,0}, {1,0,0} },
+                { {0,0,1}, {0,1,0}, {1,0,0} }
+
+            };
+            int[,,] result = new int[3, 3, 3];
+
+            for (int x = 0; x < myArray.GetLength(0); x++)
+            {
+                for (int y = 0; y < myArray.GetLength(1); y++)
+                {
+                    for (int z = 0; z < myArray.GetLength(2); z++)
+                    {
+                        result[x,y,z] += myArray[x,y,z] * arrayTwo[x, y, z];
+                        Console.WriteLine(result[x,y,z]);
+                        
+                    }
+                }
+            }
+
+
+            Console.WriteLine("Hello, World");
+
+        }
+    }
+}
